3c2bf8dad2c4d4aae3d6ae03ed5b0c9098506826
Mauricio Javier Ruiz Torres <xmaurixx_r95@hotmail.es>
2020-09-22 15:29:28 -0300

    Agregar archivos de proyecto.

---
+++ SocialMedia.Infrastructure/Repositories/PostMongoRepository.cs
@@ -0,0 +1,31 @@
+﻿using SocialMedia.Core.Entities;
+using SocialMedia.Core.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SocialMedia.Infrastructure.Repositories
+{
+    /// <summary>
+    /// clase de prueba para un repository
+    /// </summary>
+    public class PostMongoRepository /* : IPostRepository*/
+    {
+        public async Task<IEnumerable<PostPrueba>> GetPostsMongo()
+        {
+            var posts = Enumerable.Range(1, 10).Select(x => new PostPrueba
+            {
+                PostId = x,
+                Description = $"Description Mongo{x}",
+                Date = DateTime.Now,
+                Image = $"https://misapis.com/{x}",
+                UserId = x * 2
+            });
+            await Task.Delay(10);
+
+            return posts;
+        }
+    }
+}
---
+++ SocialMedia.Infrastructure/Repositories/PostRepository.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SocialMedia.Core.Entities;
+using SocialMedia.Core.Interfaces;
+using SocialMedia.Infrastructure.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SocialMedia.Infrastructure.Repositories
+{
+    public class PostRepository : IPostRepository
+    {
+        private readonly SocialMediaContext _socialMediaContext;
+        public PostRepository(SocialMediaContext socialMediaContext)
+        {
+            _socialMediaContext = socialMediaContext;
+        }
+        public async Task<IEnumerable<Post>> GetPosts()
+        {
+            var posts = await _socialMediaContext.Posts.ToListAsync();
+            return posts;
+        }
+    }
+}
